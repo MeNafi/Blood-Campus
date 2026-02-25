5f5e762bbaeac98fef97352c1e2f9fbdbedafbb2
Ahsan Habib <144062303+ahsanrafi501@users.noreply.github.com>
2026-02-25 20:51:57 +0600

    Revert "Development"

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,27 +1,9 @@
 import { createBrowserRouter } from "react-router";
-import Login from "../Pages/Auth/Login/Login";
-import HomeLayout from "../Pages/Home/HomeLayout/HomeLayout";
-import Registration from "../Pages/Auth/Registration/Registration";
-import Home from "../Pages/Home/Home/Home";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component: HomeLayout,
-    children: [
-      {
-        index:true,
-        Component: Home,
-      },
-      {
-        path:'/registration',
-        Component: Registration
-      },
-      {
-        path:'/login',
-        Component: Login,
-      }
-    ]
+    element: <div>Hello World</div>,
   },
 ]);

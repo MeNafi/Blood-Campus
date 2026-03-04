16ea7b8a9b7ddc02d9dc65a76c864ae1042af8d6
Ahsan Habib <habib23105341021@diu.edu.bd>
2026-03-04 18:52:39 +0600

    donor registration is connected with backend and data is saved in database successfully

--- src/Hook/UseAxiosSecure.jsx
+++ src/Hook/UseAxiosSecure.jsx
@@ -4,7 +4,7 @@ import UseAuth from "./UseAuth";
 import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:8000/api/v1",
 });
 
 const UseAxiosSecure = () => {

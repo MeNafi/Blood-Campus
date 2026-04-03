c7146ed6ab77de836df20ef7fe27726fb1b3ee70
Ahsan Habib <habib23105341021@diu.edu.bd>
2026-04-03 15:41:20 +0600

    filter and search added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HeroSection from '../../../components/HeroSection/HeroSection';
 import Navbar from '../../../components/Navbar/Navbar';
 import HomeCard from '../../../components/HomeCard/HomeCard';
+import BloodFilter from '../../../components/BloodFilter/BloodFilter';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <HeroSection></HeroSection>
+            <BloodFilter></BloodFilter>
             <HomeCard></HomeCard>
         </div>
     );

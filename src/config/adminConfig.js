96a10ccc5659dc256200984c649c26fc6b21c912
MeNafi <nayeefsarkernafi@gmail.com>
2026-04-10 23:13:41 +0600

    adminConfig.js file added where admin password and mail was saved

--- src/config/adminConfig.js
+++ src/config/adminConfig.js
@@ -1,5 +1,6 @@
 export const ADMIN_EMAILS = ["admin@bloodcampus.com", "habib23105341021@diu.edu.bd"];
 
+
 export const DEMO_ADMIN_CREDENTIALS = {
   email: "admin@bloodcampus.com",
   password: "Admin@123456",
